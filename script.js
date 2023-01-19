1a1fe0ddeecaabd877040c21f40511f5e162c460
Dattatray R. Suryawanshi <91654048+dattatray17@users.noreply.github.com>
2023-01-19 22:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+    const length = str.length;
+	for (let i =0; i<length/2; i++){
+		if(str[i]!== str[length-1-i]){
+			return true;
+		}
+		eles{
+			return false;
+		}
+	}
 }
 module.exports = palindrome
